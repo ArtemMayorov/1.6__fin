0e9c702b29c5aef03cddc2b68202688f0041491d
ArtemMayorov <artmai.pg@outlook.com>
2022-03-31 23:43:31 +0300

    delete comments

--- src/scss/aside/aside.js
+++ src/scss/aside/aside.js
@@ -7,7 +7,7 @@ const mainPage = document.querySelector('.main-page');
 const bodyDisable = document.querySelector('.body')
 const buttonCall = document.querySelector('.button__call')
 const buttonChat = document.querySelector('.button__chat')
-/////from call 
+
 
 const orderCall = document.querySelector('.burger-footer__call');
 const callBackPage = document.querySelector('.callback-page')
@@ -15,34 +15,29 @@ const modalBack = document.querySelector('.callback-modal__back')
 
 const openCallBack = document.querySelector('.callback-page')
 
-//// from chat
-// const buttonChat = document.querySelector('.burger-footer__chat');
+
 const openFeedback = document.querySelector('.feedback-page')
 
 
-// console.log(buttonChat)
 
 const chatPage = document.querySelector('.feedback-page')
 const feedbackButtonBack = document.querySelector('.feedback-modal__back')
-// console.log(burgerBack)
 
-// burgerBack.addEventListener('click', () => {
-//     console.log(burgerBack2)
-// })
+
+
     buttonCall.addEventListener('click', () =>{
         console.log(orderCall)
         callBackPage.classList.toggle('callback-page--active')
         mainPage.classList.add('body--disabled')
         bodyDisable.classList.add('body--disabled')
-        // openMenu.classList.toggle('aside-page--active')
+       
     })
 
 
     buttonChat.addEventListener('click', () =>{
-        // console.log(orderCall)
+   
         chatPage.classList.toggle('feedback-page--active')
-        // mainPage.classList.add('body--disabled')
-        // openMenu.classList.toggle('aside-page--active')
+     
         mainPage.classList.add('body--disabled')
         bodyDisable.classList.add('body--disabled')
     })
@@ -65,9 +60,3 @@ const feedbackButtonBack = document.querySelector('.feedback-modal__back')
             bodyDisable.classList.remove('body--disabled')
         }
     })
-// if (window.innerWidth > 360) {
-//     burgerButton.addEventListener('click', () =>{
-//         openMenu.classList.toggle('aside-page--active')
-//         bodyHidden.classList.toggle('body--disabled')
-//     })
-// }
